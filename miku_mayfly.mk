7d898401bef334c61578d2d34cee2acda077a5c6
MomoYuuki <2711300270@qq.com>
2024-12-15 21:04:58 +0800

    mayfly: Initial Miku-ui

--- miku_mayfly.mk
+++ miku_mayfly.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Miku-ui stuff.
+$(call inherit-product, vendor/miku/build/product/miku_product.mk)
 
 # Inherit from mayfly device
 $(call inherit-product, device/xiaomi/mayfly/device.mk)
@@ -18,7 +18,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := mayfly
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2206123SC
-PRODUCT_NAME := lineage_mayfly
+PRODUCT_NAME := miku_mayfly
 
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
