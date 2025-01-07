264fb89a7e31c5fa1c963de8799cc3b505823797
LuK1337 <priv.luk@gmail.com>
2025-01-07 19:41:34 +0800

    mayfly: Adjust override prop key names

--- lineage_mayfly.mk
+++ lineage_mayfly.mk
@@ -26,10 +26,11 @@ TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_SYSTEM_NAME := mayfly
 PRODUCT_SYSTEM_DEVICE := mayfly
 
-#PRODUCT_BUILD_PROP_OVERRIDES += \
-#    PRIVATE_BUILD_DESC="mayfly-user 14 UKQ1.231003.002 V816.0.11.0.ULTCNXM release-keys" \
-#    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-#    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="mayfly-user 14 UKQ1.231003.002 V816.0.11.0.ULTCNXM release-keys" \
+    BuildFingerprint=Xiaomi/mayfly/mayfly:14/UKQ1.231003.002/V816.0.11.0.ULTCNXM:user/release-keys \
+    DeviceProduct=mayfly \
+    SystemName=mayfly
 
 BUILD_FINGERPRINT := Xiaomi/mayfly/mayfly:14/UKQ1.231003.002/V816.0.11.0.ULTCNXM:user/release-keys
 
